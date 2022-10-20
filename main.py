95c4f1101e03733248f041a45bd871aa8583fea5
Onur.Kerimoglu <onur.kerimoglu@uol.de>
2022-10-20 18:06:40 +0200

    created main.py with pseudocode

---
+++ main.py
@@ -0,0 +1,24 @@
+#import packages (?)
+def main():
+    #Load the data
+
+    #Choose features
+
+    #Imputations
+    
+    #Encodings
+
+    #Visualise features (?)
+
+    #Define model
+
+    #Split Train-test
+
+    #Predict, Evaluate
+    
+    #Visualise predictions
+
+    pass
+    
+if __name__ == "__main__":
+    main()
