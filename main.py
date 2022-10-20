53a9e2e3536b31ffc7e180532410dff1d707c855
Onur.Kerimoglu <onur.kerimoglu@uol.de>
2022-10-20 23:41:37 +0200

    included imports in main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-#import packages (?)
+#import packages that will be needed by multiple modules
+import pandas as pd
+import numpy as np
+
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+import statsmodels.api as sm
+
 def main():
     #Load the data
 
