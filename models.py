f3a33a4c5b408d7dbbe6a966969395208b0573de
Onur.Kerimoglu <onur.kerimoglu@uol.de>
2022-10-21 18:31:36 +0200

    modified models.py to do DTR

--- models.py
+++ models.py
@@ -1,8 +1,11 @@
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 
 def construct_model(opt):
     if opt == 'LR':
         model = LinearRegression()
+    elif opt == 'DTR':
+        model=DecisionTreeRegressor()
     else:
         raise(ValueError(f'model:{opt} option not defined'))
         
