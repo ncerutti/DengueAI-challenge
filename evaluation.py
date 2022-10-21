c22216fa1bc710f7faf712b8161ce4d2507abe24
Onur.Kerimoglu <onur.kerimoglu@uol.de>
2022-10-22 00:29:16 +0200

    evalute: time series plot

--- evaluation.py
+++ evaluation.py
@@ -2,16 +2,27 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, mean_absolute_error,r2_score 
 
 def evaluate(y,yhat,figname='figure',visualize=True,):
+    #calculate scores
     R2, RMSE, MAE = calc_score(y,yhat) 
     print (f" R2 = {R2} \n RMSE:{RMSE} \n MAE:{MAE}")
-    fig, ax = plt.subplots(figsize=(6,6))
-    ax.plot(y,yhat,'.')
-    ax.plot([0,y.max()],[0,y.max()],'-')
-    ax.text(0.1,0.9, 'RMSE:%5.2f\nR2:%5.2f'%(RMSE,R2), transform=ax.transAxes)
-    plt.xlabel('True Sale Price')
-    plt.ylabel('Predicted Sale Price')
+    
+    #make figures
+    fig, ax = plt.subplots(2,1, figsize=(12,6))
+    plt.subplots_adjust(left=0.1,bottom=0.1,right=0.9,top=0.9,wspace=0.4,hspace=0.4)
+    #Scatter plot y vs yhat
+    ax[0].plot(y,yhat,'.')
+    ax[0].plot([0,y.max()],[0,y.max()],'-')
+    ax[0].set(xlabel='True Sale Price', ylabel='Predicted Sale Price')
+    ax[0].text(0.1,0.75, 'RMSE:%5.2f\nR2:%5.2f\nMAE:%5.2f'%(RMSE,R2,MAE), transform=ax[0].transAxes)
+    
+    #TS plot
+    ax[1].plot(range(0,len(y)),y,':b',label='true')
+    ax[1].plot(range(0,len(y)),yhat,'-r',label='predicted')
+    ax[1].set(xlabel='Week',ylabel='Sale Price')
+    plt.legend()
 
     fig.savefig(figname + '.png')
+    
     return {'R2':R2,'RMSE':RMSE,'MAE':MAE}
 
 def calc_score(y,yhat):
