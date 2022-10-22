812f86fcdb6f8698b72e2e19f5d47f16d441df6c
Onur.Kerimoglu <onur.kerimoglu@uol.de>
2022-10-22 13:05:55 +0200

    use built-in sklearn.crossval function

--- FPE.py
+++ FPE.py
@@ -1,7 +1,7 @@
 from turtle import setworldcoordinates
 from evaluation import evaluate
 from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import cross_val_score
 
 def fit_predict_evaluate(
     pl,
@@ -16,55 +16,57 @@ def fit_predict_evaluate(
     y = train_clean["total_cases"]
     X = train_clean.drop("total_cases", axis=1)
 
-    # sub-train-test split of the train dataset
-
     if crossval:
-        random_states = [42, 43, 44, 45, 46]
+        cycles=5
+        scores = cross_val_score(pl, X, y, cv=cycles, scoring='neg_mean_absolute_error')
+        print("Over %i cycles: MAE: %0.2f with std: %0.2f" % (cycles,-1*scores.mean(), -1*scores.std()))
     else:
+        # sub-train-test split of the train dataset
+        #random_states = [42,43,44,45,46]
         random_states = [42]
 
-    # cycle through different random states
-    scoresL = []
-    for i, random_state in enumerate(random_states):
-        print(f"Fit-predict cycle:{i}")
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, random_state=random_state
-        )
+        # cycle through different random states
+        scoresL = []
+        for i, random_state in enumerate(random_states):
+            print(f"Fit-predict cycle:{i}")
+            X_train, X_test, y_train, y_test = train_test_split(
+                X, y, random_state=random_state
+            )
 
-        # Fit - Predict
-        print("Fitting the model...")
-        pl.fit(X_train, y_train)
+            # Fit - Predict
+            print("Fitting the model...")
+            pl.fit(X_train, y_train)
 
-        # Evaluate
-        print("Predicting and evaluating...")
-        y_test_predict = pl.predict(X_test)
-        scoresi = evaluate(
-            y_test, y_test_predict, expname + "_" + str(i), visualize=True
-        )
-        scoresL.append(scoresi)
+            # Evaluate
+            print("Predicting and evaluating...")
+            y_test_predict = pl.predict(X_test)
+            scoresi = evaluate(
+                y_test, y_test_predict, expname + "_" + str(i), visualize=True
+            )
+            scoresL.append(scoresi)
 
-    # calculate average scores
-    keys = scoresL[0].keys()
-    scores = {}
-    for i, scoresi in enumerate(scoresL):
+        # calculate average scores
+        keys = scoresL[0].keys()
+        scores = {}
+        for i, scoresi in enumerate(scoresL):
+            for key in keys:
+                if i == 0:
+                    scores[key] = scoresi[key]
+                else:
+                    scores[key] = scores[key] + scoresi[key]
         for key in keys:
-            if i == 0:
-                scores[key] = scoresi[key]
-            else:
-                scores[key] = scores[key] + scoresi[key]
-    for key in keys:
-        scores[key] = scores[key] / len(scoresL)
+            scores[key] = scores[key] / len(scoresL)
 
-    if crossval:
-        print(
-            f"Average cross-val scores:\n R2 = {scores['R2']} \n RMSE:{scores['RMSE']} \n MAE:{scores['MAE']}"
-        )
+        if len(random_states>1):
+            print(
+                f"Averaged scores over {len(random_states)} train-test splits:\n R2 = {scores['R2']} \n RMSE:{scores['RMSE']} \n MAE:{scores['MAE']}"
+            )
 
     # Create a submission
     if create_submission:
 
         # retrain the model using the entire dataset:
-        pl.fit(X_train, y_train)
+        pl.fit(X, y)
 
         fname = expname + "_submission" + ".csv"
         submission = export_submission(test_features, test_clean, fname, pl)
